89bb018dc0b51d625e63a22258a37e7f6d49b3d4
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-02-04 21:20:38 -0300

    feat: configura o servidor para conseguir utilizar o middleware de tratamento de erros

--- src/server.js
+++ src/server.js
@@ -1,14 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const routes = require('./routes')
+const authRouter = require('./routes/auth')
+const apiRouter = require('./routes/api')
+const errorMiddleware = require('./middleware/error-middleware')
 
 //configuracao para utilizar Json
 app.use(express.json())
 
 //configuracao de rota
-app.use('/api', routes)//Ex: rotas ficam em /api/...
+app.use('/auth', authRouter)//Ex: rotas ficam em /auth/...
+app.use('/api', apiRouter)//Ex: rotas ficam em /api/...
 
+app.use(errorMiddleware)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server is running in http://localhost:${PORT}`))
