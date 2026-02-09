7ebaaedd64daee6f443b7662798220e1c4db7d7a
Gustavo Alves <gustavodevgarcia@gmail.com>
2026-02-08 22:35:49 -0300

    feat: adiciona rotas de emprestimo de livros

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const apiRouter = express.Router()
 const booksController = require('../controller/booksController')
+const loansController = require('../controller/loansController')
+const { ensureAuth } = require('../middleware/auth-middleware')
 
 apiRouter.get('/books', booksController.index)
 apiRouter.post('/books', booksController.save)
 apiRouter.get('/books/:id', booksController.show)
 apiRouter.put('/books/:id', booksController.update)
 apiRouter.delete('/books/:id', booksController.delete)
+
+apiRouter.get('/loans', loansController.index)
+apiRouter.get('/loans/:id', loansController.show)
+apiRouter.post('/loans', ensureAuth , loansController.create)
+apiRouter.post('/loans/:id/return', loansController.devolutive)
 
 module.exports = apiRouter
